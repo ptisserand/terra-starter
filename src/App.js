960b6b095a5963e92dcb320bb3944bfa984a2d30
Patrice Tisserand <p.tisserand@gmail.com>
2022-05-02 23:34:27 +0200

    Added connect/disconnect button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import './App.css';
+import { useWallet, WalletStatus } from '@terra-money/wallet-provider';
 
 function App() {
+  // current wallet status 
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      return (
+        <div className='connect-wallet-div'>
+          <button
+            type="button"
+            key={`connect-EXTENSION`}
+            onClick={() => connect("EXTENSION")}
+            className="cta-button connect-wallet-button"
+          >
+            Connect wallet
+          </button>
+        </div>
+      )
+    }
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return (
+        <button
+          type="button"
+          onClick={() => disconnect()}
+          className="cta-button connect-wallet-button"
+        >
+          Disconnect
+        </button>
+      )
+    }
+  }
+  console.log("Wallet status is:", status);
+  console.log("Available connection types:", availableConnectTypes);
 
   return (
     <main className="App">
       <header>
         <div className="header-titles">
-          <h1>⚔ Goblin War ⚔</h1>
-          <p>Only you can save us from Goblin town</p>
+          <h1>🍺 The World's End 🍺</h1>
+          <p>Only you can bring us to the last pub</p>
         </div>
 
       </header>
 
       <div>
-        <img
-          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
-          alt="Goblin gif"
+        <img id="pub-img"
+          src="https://simonpegg.net/wp-content/uploads/2013/07/theworldsendpub-707x1024.jpg"
+          alt="The World's end pub"
+
         />
       </div>
+      {renderConnectButton()}
     </main>
   );
 }
