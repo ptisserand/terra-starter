ec2eb993242f12654b37321b17252036d2b7e6a5
Patrice Tisserand <p.tisserand@gmail.com>
2022-05-03 22:48:00 +0200

    Added scores fetching

--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,5 +1,20 @@
-const LeaderBoard = () => {
+import * as query from '../contracts/query';
+import { useConnectedWallet } from '@terra-money/wallet-provider';
 
-}
+const LeaderBoard = () => {
+    const connectedWallet = useConnectedWallet();
+
+    const fetchScores = async () => {
+        if (connectedWallet && connectedWallet.network.name === 'testnet') {
+            console.log("Scores fetched:", (await query.getScores(connectedWallet)).scores);
+        }
+    }
+
+    fetchScores();
+
+    return (
+        <></>
+    );
+};
 
 export default LeaderBoard;
