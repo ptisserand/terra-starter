b5386f4e1cccf588a1ea3973e5ea45bfa101dc82
Patrice Tisserand <p.tisserand@gmail.com>
2022-05-03 23:03:53 +0200

    Added score display in leaderboard page

--- src/pages/guide.js
+++ src/pages/guide.js
@@ -1,29 +1,29 @@
 import { Link } from 'react-router-dom';
 
 const Guide = () => {
-    return (
-        <main className="App">
-        <header>
+  return (
+    <main className="App">
+      <header>
         <Link to="/" className="home-link">
-        <div className="header-titles">
-          <h1>🍺 The World's End 🍺</h1>
-          <p>Only you can bring us to the last pub</p>
-        </div>
-</Link>
+          <div className="header-titles">
+            <h1>🍺 The World's End 🍺</h1>
+            <p>Only you can bring us to the last pub</p>
+          </div>
+        </Link>
       </header>
 
-    
-          <div className="score-board-container">
-            <h3>How to play</h3>
-            
-            <div>
-              <span className="help">
-                Drink as many beers as you can within 15 seconds!
-              </span>
-            </div>
-          </div>
-        </main>
-      );
+
+      <div className="score-board-container">
+        <h3>How to play</h3>
+
+        <div>
+          <span className="help">
+            Drink as many beers as you can within 15 seconds!
+          </span>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Guide;
--- src/pages/leaderboard.js
+++ src/pages/leaderboard.js
@@ -1,19 +1,65 @@
+import { Link } from 'react-router-dom';
 import * as query from '../contracts/query';
 import { useConnectedWallet } from '@terra-money/wallet-provider';
+import { useState, useEffect } from 'react';
+
 
 const LeaderBoard = () => {
+    const [scores, setScores] = useState();
+    const [loading, setLoading] = useState(true);
     const connectedWallet = useConnectedWallet();
 
-    const fetchScores = async () => {
-        if (connectedWallet && connectedWallet.network.name === 'testnet') {
-            console.log("Scores fetched:", (await query.getScores(connectedWallet)).scores);
-        }
-    }
+    useEffect(()=> {
+        setLoading(true);
+        const fetchScores = async () => {
+            if (connectedWallet && connectedWallet.network.name === 'testnet') {
+                return (await query.getScores(connectedWallet)).scores;
+            }
+        };
+        
+        fetchScores().then(scores => {
+            setScores(scores);
+            setLoading(false);
+        })
+    }, [connectedWallet]);
 
-    fetchScores();
+    const renderScores = (scores) => {
+        if (!scores || scores.length < 1) {
+            return <div>No scores available :(</div>
+        }
+
+        return scores.map((score, index) => {
+            return(
+                <div key={index} className="score">
+                    <span>
+                        {score[0].slice(0,5) + '...' + score[0].slice(-4)}:{' '}
+                        {score[1].toString().padStart(2, '0')}
+                    </span>
+                </div>
+            );
+        });
+    };
 
     return (
-        <></>
+        <main className="App">
+        <header>
+          <Link to="/" className="home-link">
+            <div className="header-titles">
+              <h1>🍺 The World's End 🍺</h1>
+              <p>Only you can bring us to the last pub</p>
+            </div>
+          </Link>
+        </header>
+        <div className="score-board-container">
+            <h3>Scoreboard</h3>
+            {loading ? (
+                <div>Loading....</div>
+            ): (
+                renderScores(scores)
+            )}
+            <div></div>
+        </div>
+        </main>
     );
 };
 
